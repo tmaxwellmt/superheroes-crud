175dd0fef78766c12825565c33c5ccceeb357a1c
tmaxwellmt <tmaxwellmt@gmail.com>
2017-03-29 21:25:49 -0600

    posted first superhero using body parser for data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var Superhero = require('./models/superhero');
-
 var app = express();
+var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 
@@ -9,26 +9,37 @@ var mongoose = require('mongoose');
 //it will look for"/" or create a db called superheroes.
 mongoose.connect('mongodb://localhost/superheroes');
 
-//app.METHOD('URL LOCATION, fnction(req, res)')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/test', function(req, res) {
-  res.send("You found a test router");
+app.get('/superheroes', function(req, res) {
+
+  Superhero.find(function(err, data) {
+    if(err){
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+
 });
 
-// var server = app.listen(3000, function() {
-//   console.log('Server running on PORT 3000');
-// });
-var superHero1 = {
-  name: "superman",
-  superPower: "strong",
-  universe: "DC",
-  evil: false,
-  rank: 10
-};
-
-app.get('/superhero', function (req, res) {
-  res.json(superHero1);
-});
+app.post('/superheroes', function(req, res){
+  var newSuper = new Superhero({
+    name: req.body.name,
+    superPower: req.body.superPower,
+    universe: req.body.universe,
+    evil: req.body.evil,
+    rank: req.body.rank,
+  });
+  newSuper.save(function(err, sh) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(sh)
+    }
+  });
+})
 
 var server = app.listen(3000, function () {
   console.log('server is running');
